0ca79c629887f33823ef35051041ccfbcad549e5
LISH5 <727904507@qq.com>
2020-07-23 19:26:18 +0800

    [GuessNumberGame][ShaoLi] feature:add should_return_wrong_message_when_guess_given_1134 test.

--- src/test/java/junit/tdd/GuessNumberGameTest.java
+++ src/test/java/junit/tdd/GuessNumberGameTest.java
@@ -123,4 +123,20 @@ public class GuessNumberGameTest {
         //then
         assertEquals("Wrong Input,Input again", guessResult);
     }
+
+    @Test
+    void should_return_wrong_message_when_guess_given_1134() {
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        when(answerGenerator.generateAnswer()).thenReturn("1234");
+
+        String inputNumber = "1134";
+        String answer = answerGenerator.generateAnswer();
+        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+
+        //when
+        String guessResult = guessNumberGame.guess(inputNumber);
+
+        //then
+        assertEquals("Wrong Input,Input again", guessResult);
+    }
 }
