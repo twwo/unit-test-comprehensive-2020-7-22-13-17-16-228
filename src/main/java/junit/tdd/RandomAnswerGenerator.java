ce6e3618448b194f510535d5ffe65f6817b76842
LISH5 <727904507@qq.com>
2020-07-23 19:42:06 +0800

    [GuessNumberGame][ShaoLi] refactor:implement RandomAnswerGenerator.generateAnswer can return a random string which size is 4 and no repeating and number' value is between 0 to 9.

--- src/main/java/junit/tdd/RandomAnswerGenerator.java
+++ src/main/java/junit/tdd/RandomAnswerGenerator.java
@@ -1,28 +1,19 @@
 package junit.tdd;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class RandomAnswerGenerator implements AnswerGenerator {
-    private List<Integer> numberSet;
-
-    public RandomAnswerGenerator() {
-        numberSet = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            numberSet.add(i);
-        }
-    }
 
     @Override
     public String generateAnswer() {
         Random random = new Random();
-        String answer = "1234";
-        return answer;
-    }
-
-    public static void main(String[] args) {
-        AnswerGenerator answerGenerator = new RandomAnswerGenerator();
-        System.out.println(answerGenerator.generateAnswer());
+        StringBuilder answer = new StringBuilder();
+        do {
+            String randomNumber = String.valueOf(random.nextInt(10));
+            if (!answer.toString().contains(randomNumber)) {
+                answer.append(randomNumber);
+            }
+        } while (answer.length() != 4);
+        return answer.toString();
     }
 }
