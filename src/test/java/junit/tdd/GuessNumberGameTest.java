06d318ac814dc87963c1a0cec1b049936498892a
LISH5 <727904507@qq.com>
2020-07-23 18:18:11 +0800

    [GuessNumberGame][ShaoLi] feature:add should_return_1A3B_when_guess_given_1349_and_answer_is_1234 test.

--- src/test/java/junit/tdd/GuessNumberGameTest.java
+++ src/test/java/junit/tdd/GuessNumberGameTest.java
@@ -22,4 +22,21 @@ public class GuessNumberGameTest {
         //then
         assertEquals("4A0B", guessResult);
     }
+
+    @Test
+    void should_return_1A3B_when_guess_given_1349_and_answer_is_1234() {
+        //given
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        when(answerGenerator.generateAnswer()).thenReturn("1234");
+
+        String inputNumber = "1349";
+        String answer = answerGenerator.generateAnswer();
+        GuessNumberGame guessNumberGame = new GuessNumberGame(answer);
+
+        //when
+        String guessResult = guessNumberGame.guess(inputNumber);
+
+        //then
+        assertEquals("1A3B", guessResult);
+    }
 }
