03c5efed0f14e9fccd1b0a591289f5a1f099a75b
LISH5 <727904507@qq.com>
2020-07-27 19:28:31 +0800

    [GuessNumberGame][ShaoLi] refactor: rename inputNumber to inputGuessNumber

--- src/main/java/junit/tdd/GuessNumberGame.java
+++ src/main/java/junit/tdd/GuessNumberGame.java
@@ -10,17 +10,17 @@ public class GuessNumberGame {
         this.answer = answer;
     }
 
-    public String guess(String inputNumber) {
-        return calculateAandB(inputNumber);
+    public String guess(String inputGuessNumber) {
+        return calculateAandB(inputGuessNumber);
     }
 
-    private String calculateAandB(String inputNumber) {
-        if (!isValid(inputNumber)) {
+    private String calculateAandB(String inputGuessNumebr) {
+        if (!isValid(inputGuessNumebr)) {
             return "Wrong Input,Input again";
         }
         int aCount = 0;
         int bCount = 0;
-        char[] inputNumbers = inputNumber.toCharArray();
+        char[] inputNumbers = inputGuessNumebr.toCharArray();
         char[] answerNumbers = answer.toCharArray();
         for (int pos = 0; pos < inputNumbers.length; pos++) {
             if (answerNumbers[pos] == inputNumbers[pos]) {
@@ -33,18 +33,18 @@ public class GuessNumberGame {
         return aCount + "A" + bCount + "B";
     }
 
-    private boolean isValid(String inputNumber) {
-        Validator validator = new InputNumberValidator(inputNumber);
+    private boolean isValid(String inputGuessNumber) {
+        Validator validator = new InputNumberValidator(inputGuessNumber);
         return validator.validate();
 //        boolean isValid = true;
-//        if (!inputNumber.matches("-?[0-9]+(\\.[0-9]+)?")) {
+//        if (!inputGuessNumber.matches("-?[0-9]+(\\.[0-9]+)?")) {
 //            isValid = false;
 //        }
-//        if (inputNumber.length() != 4) {
+//        if (inputGuessNumber.length() != 4) {
 //            isValid = false;
 //        } else {
-//            for (char number : inputNumber.toCharArray()) {
-//                if (inputNumber.indexOf(number) != inputNumber.lastIndexOf(number)) {
+//            for (char number : inputGuessNumber.toCharArray()) {
+//                if (inputGuessNumber.indexOf(number) != inputGuessNumber.lastIndexOf(number)) {
 //                    isValid = false;
 //                    break;
 //                }
