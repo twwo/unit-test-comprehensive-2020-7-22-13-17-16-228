5aa8b6f9a6bec1074c8c282510580f297d180b74
LISH5 <727904507@qq.com>
2020-07-28 08:17:55 +0800

    [GuessNumberGame][ShaoLi] refactor: delete useless import

--- src/test/java/junit/tdd/RandomAnswerGeneratorTest.java
+++ src/test/java/junit/tdd/RandomAnswerGeneratorTest.java
@@ -2,7 +2,6 @@ package junit.tdd;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RandomAnswerGeneratorTest {
